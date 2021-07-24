80805153c07c0f9d18731107de5a0dc74e752fde
Toan Quoc Ho <hqtoan94@gmail.com>
2021-07-24 20:43:17 +0700

    Update copydb.sh

--- copydb.sh
+++ copydb.sh
@@ -1,1 +1,10 @@
+# Copy DB
 PGPASSWORD=<> pg_dump -C -h <src_hostname> -U <src_username> <src_db> -p <src_port> -O -x | psql -h <dest_host> -U <dest_username> <dest_db>
+
+
+# Export DB into file
+PGPASSWORD=<> pg_dump -C -h <src_hostname> -U <src_username> <src_db> -p <src_port> -O -x > archieve.sql
+
+
+#Import DB from file
+psql -h <src_hostname> -U <src_username> -p <src_port> <src_db> -f archieve.sql
